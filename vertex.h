7bbca0a32561cd826e791fdb759e197ee237134f
haffent1 <50844313+haffent1@users.noreply.github.com>
2021-04-27 09:51:25 -0400

    Add files via upload

---
+++ vertex.h
@@ -0,0 +1,23 @@
+
+
+//vertex.h
+
+#ifndef VERTEX_H
+#define VERTEX_H
+
+#include <iostream>
+using namespace std;
+
+
+
+
+class KeyError{};
+class SizeError{};
+class CopyError{};
+
+#include "List.cpp"
+#endif
+
+
+
+
