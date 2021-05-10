4adaed6c5eb624139dd1700b9dcdcddc7e1720c8
haffent1 <50844313+haffent1@users.noreply.github.com>
2021-05-10 18:24:54 -0400

    Add files via upload

--- testGraph.cpp
+++ testGraph.cpp
@@ -5,17 +5,15 @@
 using namespace std;
 
 void testGraphConstructors();
-void testGet();
+void testGetWeight();
 void testDFS();
 void testCycle();
 void testPrim();
-void testList();
 
-int main(  void  )
+int main(void)
 {
-  //testList();
   testGraphConstructors();
-  //testGet();
+  testGetWeight();
   //testDFS();
   //testCycle();
   testPrim();
@@ -25,17 +23,18 @@ int main(  void  )
 
 void testGraphConstructors()
 {
-
     //also test reading in a wrong file name
     //also test reading in a larger graph
     //also test reading in a graph with double digit weights
-
     cout<<"test base constructor"<<endl;
     //Correct constructor test
     Graph g1("exampleGraph.txt");
     cout << "graph g1: " << g1 << endl;
     
-    
+    cout<<"test base constructor for multi digit"<<endl;
+    Graph gMultiDigit("exampleGraph2Digit.txt");
+    cout << "graph MultiDigit: " << gMultiDigit << endl;
+
     cout<<"test copy constructor"<<endl;
     Graph g2(g1);
     cout << "graph g2: " << g2 << endl;
@@ -46,15 +45,15 @@ void testGraphConstructors()
     cout<<endl;
     
     //g1.~Graph();
-}
+}//end of constructors
 
-
-void testGet()
+void testGetWeight()
 {
     //test getting weights that dont exist 
-    cout<<"test = overload constructor"<<endl;
+    cout<<"test getWeight"<<endl;
     Graph g4("exampleGraph.txt");
-    cout << "graph g4: " << g4 << endl;
+    //cout << "graph g4: " << g4 << endl;
+
     cout << "getWeight g4 (4,2) : " << g4.getWeight(4, 2) << endl;
     cout << "getWeight g4 (0,2) : " << g4.getWeight(0, 2) << endl;
     cout << "getWeight g4 (4,3) : " << g4.getWeight(4, 3) << endl;
@@ -64,12 +63,24 @@ void testGet()
 }
 
 void testDFS()
-{
-
+{ 
+    cout<<"test dfs"<<endl;
+    Graph testdfs("exampleGraph.txt");
+    testdfs.dfs();
+    Graph testDfsDisJointed("exampleGraphDisJointed.txt"); //test if the the print shows the disjointed
+    testDfsDisJointed.dfs();
+    cout<<endl;
 }
 
 void testCycle()
 {
+    cout<<"test Cycle"<<endl;
+    Graph testCycleYes("exampleGraph.txt");
+    bool cycle = testCycleYes.cycle();
+    if(cycle == true){
+        cout<<"cycle found in graph with cycle"<<endl;
+    }
+
 
 }
 
@@ -77,6 +88,8 @@ void testPrim()
 {
     cout<<"test Prim"<<endl;
     Graph testPrimGraph("exampleGraph.txt");
+    cout<<"should be:"<<endl<<"0->1"<<endl<<"1->2"<<endl<<"1->3"<<endl<<"3->4"<<endl;
+    cout<<"real:"<<endl;
     testPrimGraph.Prim(0);
     cout<<endl;
     
